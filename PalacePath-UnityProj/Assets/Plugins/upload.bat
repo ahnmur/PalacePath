40f1848e92fddf3d7a18749f56c367878f28c968
ahnmur <contactannmur@gmail.com>
2020-10-23 20:43:00 -0400

    Added  Unity Project

--- PalacePath-UnityProj/Assets/Plugins/upload.bat
+++ PalacePath-UnityProj/Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@D:\Dropbox\_PALACE PATH\Palace\PalacePath-UnityProj\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1574449222843892730.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@D:\Dropbox\_PALACE PATH\Palace\PalacePath-UnityProj\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1590245384016364396.json
